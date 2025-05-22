b99d3ef04c9e6e6ff0a2e81263702f3f1270d663
joaofelipegalvao <176946005+joaofelipegalvao@users.noreply.github.com>
2025-05-22 17:09:38 -0300

    feat(java): melhora suporte a projetos com Maven/Gradle e ignora arquivos irrelevantes

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,4 +3,27 @@
 -- Add any additional options here
 
 vim.opt.spelllang = { "en", "pt" }
+vim.opt.title = true
+vim.opt.showcmd = true
 vim.opt.spell = true
+vim.g.root_spec = {
+  "lsp",
+  { ".git", "pom.xml", "build.gradle", "build.gradle.kts", "settings.gradle", "settings.gradle.kts", "lua" },
+  "cwd",
+}
+vim.opt.breakindent = true
+vim.opt.path:append({ "**" })
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.wildignore:append({
+  "*/.git/*",
+  "*/.idea/*",
+  "*/target/*",
+  "*/build/*",
+  "*.class",
+  "*.jar",
+  "*.war",
+  "*log",
+  "*.tmp",
+  "*/node_modules/*",
+  "*/dist/*",
+})
