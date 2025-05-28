95cf3b5ad1c30042b45ebc386f038774a0cb0f8f
joaofelipegalvao <176946005+joaofelipegalvao@users.noreply.github.com>
2025-05-28 07:00:33 -0300

    feat(ui): configura ícones personalizados para desenvolvimento Java
    - Adiciona ícones especializados para:
      1 Arquivos de configuração do Eclipse (.prefs, .project, .classpath)
      2 Arquivos Maven (pom.xml, mvnw, wrapper properties)
      3 Arquivos Java (.java)
      4 Configuração Spring (application.properties)
      5 Diretórios de IDE (.settings, .idea)
    - Define ícones personalizados para:
      1 Documentação README.md
      2 Scripts executáveis (mvnw.cmd)
    - Utiliza mini.icons para personalização leve de ícones

---
+++ nvim/.config/nvim/lua/plugins/devicons.lua
@@ -0,0 +1,29 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    file = {
+      ["org.eclipse.core.resources.prefs"] = { glyph = "󰦨" },
+      ["org.eclipse.jdt.apt.core.prefs"] = { glyph = "󰦨" },
+      ["org.eclipse.jdt.core.prefs"] = { glyph = "󰦨" },
+      ["org.eclipse.m2e.core.prefs"] = { glyph = "󰦨" },
+      ["maven-wrapper.properties"] = { glyph = "" },
+      [".project"] = { glyph = "" },
+      [".classpath"] = { glyph = "" },
+      ["pom.xml"] = { glyph = "" },
+      ["mvnw"] = { glyph = "" },
+      ["mvnw.cmd"] = { glyph = "󰦪" },
+      ["application.properties"] = { glyph = "" },
+
+      ["README.md"] = { glyph = ""}
+    },
+
+    extension = {
+      ["java"] = { glyph = "󰅶" },
+    },
+
+    directory = {
+      [".settings"] = { glyph = "󱁽" },
+      [".idea"] = { glyph = "" },
+    }
+  }
+}
