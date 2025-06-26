cd0b13cb8f421d580f966cd6190ec667e62cf7d8
joaofelipegalvao <176946005+joaofelipegalvao@users.noreply.github.com>
2025-06-26 19:53:19 -0300

    config(nvim): adicionconfig(nvim): adiciona suporte a Rust e TOML via LazyVim

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -29,6 +29,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.editor.dial" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "plugins" },
   },
   defaults = {
