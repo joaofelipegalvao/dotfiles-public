8e7a9d3b9952562eed9f2931ca6ab4bdd96868c7
joaofelipegalvao <176946005+joaofelipegalvao@users.noreply.github.com>
2025-08-28 12:18:32 -0300

    refactor(shell): reorganiza config do Fish e adiciona setup do ssh-agent

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,5 @@
 set fish_greeting ""
 
-
 set -gx COLORTERM truecolor
 set -gx TERM xterm-256color
 
@@ -23,42 +22,25 @@ alias g='git'
 alias lzd="lazydocker"
 alias lzg="lazygit"
 
-# ---- Mise ----
-/usr/bin/mise activate fish | source
-
-
+# ---- PATHs ----
 set -x PATH $HOME/.local/bin $HOME/.scripts $PATH
+
+# ---- Mise ----
+mise activate fish | source
 
 # ---- Starship ----
 starship init fish | source
 
-
+# ---- Editor ----
 command -qv nvim && alias vim nvim
-
 set -gx EDITOR nvim
 
 # ---- Theme FZF ----
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
-  --highlight-line \
-  --info=inline-right \
-  --ansi \
-  --layout=reverse \
-  --border=none \
-  --color=bg+:#2d3f76 \
-  --color=bg:#1e2030 \
-  --color=border:#589ed7 \
-  --color=fg:#c8d3f5 \
-  --color=gutter:#1e2030 \
-  --color=header:#ff966c \
-  --color=hl+:#65bcff \
-  --color=hl:#65bcff \
-  --color=info:#545c7e \
-  --color=marker:#ff007c \
-  --color=pointer:#ff007c \
-  --color=prompt:#65bcff \
-  --color=query:#c8d3f5:regular \
-  --color=scrollbar:#589ed7 \
-  --color=separator:#ff966c \
-  --color=spinner:#ff007c \
-"
+set -gx FZF_DEFAULT_OPTS "--highlight-line --info=inline-right --ansi --layout=reverse --border=none --color=bg+:#2d3f76,bg:#1e2030,border:#589ed7,fg:#c8d3f5,gutter:#1e2030,header:#ff966c,hl+:#65bcff,hl:#65bcff,info:#545c7e,marker:#ff007c,pointer:#ff007c,prompt:#65bcff,query:#c8d3f5:regular,scrollbar:#589ed7,separator:#ff966c,spinner:#ff007c"
 
+
+if not pgrep -f ssh-agent > /dev/null
+    eval (ssh-agent -c) > /dev/null
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
